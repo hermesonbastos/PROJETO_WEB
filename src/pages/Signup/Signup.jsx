a6f56d37296b471a4d6f027bd5a2a8b968b1cbc1
hermesonbastos <hermesonbastos@gmail.com>
2024-09-22 19:17:14 -0300

    feat: menu theme

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
 import useForm from '../../hooks/useForm';
@@ -9,8 +10,11 @@ const Signup = () => {
   const email = useForm("email");
   const password = useForm("password");
 
-  const handleSubmit = () => {
+  const navigate = useNavigate();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/feed");
   }
 
   return <div className='signup'>
@@ -20,8 +24,8 @@ const Signup = () => {
         <Input label="nome" type="text" name="username" {...name} />
         <Input label="email" type="email" name="email" {...email} />
         <Input label="senha" type="password" name="password" {...password} />
+        <Button className="btn-primary" label="CONHECER" />
       </form>
-      <Button className="btn-primary" label="CONHECER" />
     </div>
   </div>
 }
